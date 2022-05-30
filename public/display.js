a48253e7f98a7609cc9928d52091661782a0eb31
Freya Arbjerg <freya@arbjerg.dev>
2022-05-30 23:59:04 +0200

    Add working clock

---
+++ public/display.js
@@ -0,0 +1,34 @@
+import { createApp, reactive } from "https://unpkg.com/petite-vue@0.2.2/dist/petite-vue.es.js"
+
+const store = reactive({
+    clock: "",
+    date: "",
+    setClock(clock) {
+        this.clock = clock
+    },
+    setDate(date) {
+        this.date = date
+    }
+})
+
+createApp({
+    store
+}).mount("#mount")
+
+function updateClock() {
+    const date = new Date()
+    store.setClock(date.getHours() + ":" + date.getMinutes())
+    const dateOfMonth = date.getDate()
+
+    let ordinalIndicator
+    if (dateOfMonth % 10 === 1) ordinalIndicator = "ˢᵗ"
+    else if (dateOfMonth % 10 === 2) ordinalIndicator = "ⁿᵈ"
+    else if (dateOfMonth % 10 === 3) ordinalIndicator = "ʳᵈ"
+    else ordinalIndicator = "ᵗʰ"
+
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December"]
+
+    store.setDate(months[date.getMonth()] + " " + date.getDate() + ordinalIndicator + " " + date.getFullYear())
+}
+
+setInterval(updateClock, 1000)
